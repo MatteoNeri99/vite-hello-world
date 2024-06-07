13e97e8c195dcbf280802ce497207def304dbf46
MatteoNeri99 <matteolavoro99@gmail.com>
2024-06-07 13:07:04 +0200

    aggiunto AppTitleEl in template

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+ <h1>La mia prima App con vite</h1>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
